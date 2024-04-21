3d4b3175e44a4452342998d382fa2dd220b23631
iwis-zcas <iwis.zhou@caspianone.com>
2024-04-22 00:57:22 +0800

    update page 3

--- frontend/src/Items.js
+++ frontend/src/Items.js
@@ -8,44 +8,79 @@ import { Box, Rating, Typography } from "@mui/material";
 import NextButton from "./SubmitButton";
 import { useLocation, useNavigate } from "react-router-dom";
 import { fetchRecommendGames } from "./APIs";
+import { getDefaultUserProfile } from "./data";
 
 const generateNewUserId = () => {
   return 100000 + parseInt(Math.random() * 10);
 };
 
+const SessionUserId = generateNewUserId();
+
 const methodExplanation = {
-  content_based: "These games have similar tags as what you selected",
-  svd: "These games are recommended to you based on players who have the same preferences as you",
+  content_based: {
+    text: "These games have similar tags as what you selected",
+    color: "#FC6238",
+  },
+  svd: {
+    text: "These games are recommended to you based on players who have the same preferences as you",
+    color: "#FFD872",
+  },
 };
 
 export default function Items() {
   const navigate = useNavigate();
   const location = useLocation();
   const [items, setItems] = useState([]);
+  const [userProfiles, setUserProfiles] = useState([]);
 
   let userLikes = location?.state?.["user-likes"] || "";
   console.log(userLikes);
 
   useEffect(() => {
-    const userId = generateNewUserId();
-    const userProfiles = userLikes.map((l) => ({
-      user_id: userId,
-      game_name: l.name,
-      game_id: l.game_id,
-      rating: 3,
-    }));
+    let userProfiles;
+    if (!userLikes || userLikes?.length === 0) {
+      //Default user profile
+      userProfiles = getDefaultUserProfile(SessionUserId);
+    } else {
+      userProfiles = userLikes.map((l) => ({
+        user_id: SessionUserId,
+        game_name: l.name,
+        game_id: l.game_id,
+        rating: 3,
+      }));
+    }
     fetchRecommendGames(userProfiles).then((data) => {
       console.log(data);
       setItems(data);
+      setUserProfiles(userProfiles);
     });
   }, []);
 
-  const handleRateItemOnClick = (itemIndex) => (event, newValue) => {
-    const cp = [...items];
-    cp[itemIndex] = { ...cp[itemIndex] };
-    cp[itemIndex].all_reviews_score = newValue;
-    setItems(cp);
-  };
+  const handleRateItemOnClick =
+    (methodName, itemIndex) => (event, newValue) => {
+      const cp = { ...items };
+      cp[methodName][itemIndex] = { ...cp[methodName][itemIndex] };
+      cp[methodName][itemIndex].all_reviews_score = newValue;
+      console.log(cp[methodName][itemIndex]);
+      setItems(cp);
+
+      //Update user profile
+      const cpUserProfiles = [...userProfiles];
+      cpUserProfiles.push({
+        user_id: SessionUserId,
+        game_name: cp[methodName][itemIndex].name,
+        game_id: cp[methodName][itemIndex].game_id,
+        rating: newValue,
+      });
+      console.log(cpUserProfiles);
+      setUserProfiles(cpUserProfiles);
+
+      //Fetch new Recommendation
+      fetchRecommendGames(cpUserProfiles).then((data) => {
+        console.log(data);
+        setItems(data);
+      });
+    };
   return (
     <Box>
       <Typography
@@ -66,18 +101,20 @@ export default function Items() {
         return (
           <>
             <Typography
-              variant="h4"
+              variant="h6"
               gutterBottom
               sx={{
                 //   backgroundColor: "#fee7da",
-                padding: "30px 100px 50px 100px",
+                // padding: "30px 100px 50px 0px",
+                paddingLeft: "5px",
                 fontWeight: "400",
-                textAlign: "center",
+                textAlign: "left",
                 //   borderRadius: "5px",
                 color: "white",
+                borderLeft: `7px solid ${methodExplanation[m].color}`,
               }}
             >
-              {methodExplanation[m]}
+              {methodExplanation[m].text}
             </Typography>
             <ImageList cols={5} gap={2}>
               {items[m].map((i, index) => (
@@ -110,7 +147,7 @@ export default function Items() {
                           }
                           name="simple-controlled"
                           value={i.all_reviews_score}
-                          onChange={handleRateItemOnClick(index)}
+                          onChange={handleRateItemOnClick(m, index)}
                         />
                       </IconButton>
                     }
@@ -118,6 +155,7 @@ export default function Items() {
                 </ImageListItem>
               ))}
             </ImageList>
+            <br />
           </>
         );
       })}
---
+++ frontend/src/data.js
@@ -0,0 +1,8 @@
+export const getDefaultUserProfile = (userId) => [
+  {
+    user_id: userId,
+    game_name: "Call of Duty®: Black Ops",
+    game_id: 42700,
+    rating: 3,
+  },
+];
